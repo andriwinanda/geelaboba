d7f3013effe63e6ce714cf9891204448e3809952
andriwinanda <andriwinanda98@gmail.com>
2021-10-24 19:53:50 +0700

    update voucher

--- src/pages/voucher.vue
+++ src/pages/voucher.vue
@@ -23,32 +23,5 @@
         </f7-card-content>
       </f7-card>
     </div>
-    <f7-block v-if="products.length === 3">
-      <f7-button fill @click="addProduct">Add Product</f7-button>
-    </f7-block>
   </f7-page>
 </template>
-<script>
-import { useStore } from "framework7-vue";
-import store from "../js/store";
-
-export default {
-  setup() {
-    const products = useStore("products");
-
-    const addProduct = () => {
-      store.dispatch("addProduct", {
-        id: "4",
-        title: "Apple iPhone 12",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.",
-      });
-    };
-
-    return {
-      products,
-      addProduct,
-    };
-  },
-};
-</script>
